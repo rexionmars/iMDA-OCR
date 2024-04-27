1a4293f14f74d62a05a517a904f779fa062d5dc7
rexionmars <opensource.leonardi@gmail.com>
2024-04-27 19:14:08 -0300

    feat: Argument parse

--- engine/core.py
+++ engine/core.py
@@ -1,9 +1,18 @@
 import sys
+import argparse
 from ocr.TextRecognition import TextRecognition
 
-if __name__ == "__main__":
-    text_recognition = TextRecognition(0)  # Altere para o índice da sua câmera, se necessário
+
+def main(camera_index):
+    text_recognition = TextRecognition(camera_index)
     text_recognition.start()
     while text_recognition.running:
         pass  # Aguarde até que a tecla "q" ou "ESC" seja pressionada
     sys.exit(0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script para reconhecimento de texto com índice da câmera')
+    parser.add_argument('--camera-index', type=int, default=0, help='Índice da câmera (padrão: 0)')
+    args = parser.parse_args()
+    main(args.camera_index)
