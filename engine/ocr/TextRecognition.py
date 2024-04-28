bd746f2e0e968736393a066ba112db8ba74e61ec
rexionmars <opensource.leonardi@gmail.com>
2024-04-28 17:07:40 -0300

    ref: Refactor OCR logic to improve ROI data processing and add debug flag control for CV2 visual geometric detectors and text recognition in TextRecognition.py

--- engine/ocr/TextRecognition.py
+++ engine/ocr/TextRecognition.py
@@ -13,6 +13,10 @@ from common.FloatingRectangle import FloatingRectangle
 from common.VideoCapture import VideoCapture
 from common.geometrics import BasicGeometrics
 
+from configurations.debug_flag_control import ENABLE_VISUAL_GEOMETRIC_DETECTORS, ENABLE_DEBUG_ROI_INFO
+from configurations.constants import DEFAULT_COLOR_DETECTION_TEXT as GREEN
+from configurations.constants import DEAFULT_COLOR_DETECTION_RECTANGLE as YELLOW
+
 
 class TextRecognition:
 
@@ -32,7 +36,7 @@ class TextRecognition:
             "Select the Information Unit", "Select the label", "Select the main value", "Select the minimum value",
             "Select the maximum value"
         ]
-        self.show_floating_rectangle = True  # Flag para mostrar ou ocultar o retângulo flutuante
+        self.show_floating_rectangle = True
         self.floating_rectangle = FloatingRectangle('Text Recognition')
         self.delayed_processing_thread = threading.Thread(target=self.delayed_processing)
         self.delayed_processing_thread.daemon = True
@@ -45,24 +49,23 @@ class TextRecognition:
         # Adicione os valores filtrados à fila de processamento
         self.event_queue.put((unit_name, filtered_values))
 
-    def delayed_processing(self):
+    def delayed_processing(self) -> None:
         while self.running:
             # Verifique se há valores na fila de processamento
             if not self.event_queue.empty():
                 # Obtenha os valores da fila
                 unit_name, filtered_values = self.event_queue.get()
 
-                # Processamento dos valores filtrados aqui
                 unit_base_values = {
                     "current": filtered_values[0] if filtered_values else 0,
                     "min": filtered_values[1] if len(filtered_values) > 1 else 0,
                     "max": filtered_values[2] if len(filtered_values) > 2 else 0,
                 }
-                print(f"[INFO] Unit {unit_name}: {unit_base_values}")
 
-                # Simular um atraso de 1 segundo
-                time.sleep(1)
-                self.print_roi_data(unit_name, filtered_values)
+                time.sleep(1)  # Delay para controlar a taxa de processamento
+                if ENABLE_DEBUG_ROI_INFO:
+                    print(f"[INFO] Unit {unit_name}: {unit_base_values}")
+                    self.print_roi_data(unit_name, filtered_values)
                 #return unit_name, unit_base_values
 
     def read_text(self, frame):
@@ -88,13 +91,16 @@ class TextRecognition:
                     text_x = x + bbox[0][0]
                     text_y = y + bbox[0][1]
 
-                    # Desenha o texto e o retângulo ao redor do texto reconhecido
-                    #cv2.rectangle(frame, (text_x, text_y), (x + bbox[2][0], y + bbox[2][1]), (0, 255, 0), 1)
+                    if ENABLE_VISUAL_GEOMETRIC_DETECTORS:
+                        # Desenha o texto e o retângulo ao redor do texto reconhecido
+                        cv2.rectangle(frame, (text_x, text_y), (x + bbox[2][0], y + bbox[2][1]), GREEN, 1)
+                        cv2.putText(frame, text, (text_x, text_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, GREEN, 1)
 
                 # Remove valores nulos da lista
                 filtered_values = [float(item) for item in list_value if item is not None]
                 #self.print_roi_data(label, filtered_values)
 
+                # ROI
                 self.geometric.rounded_rectangle(frame, (x, y, w, h), thickness_of_line=1)
 
         self.last_frame = frame
@@ -130,6 +136,7 @@ class TextRecognition:
                     for roi in self.rois:
                         self.geometric.rounded_rectangle(frame, roi, thickness_of_line=1)
                 self.read_text(frame)
+
                 if self.show_floating_rectangle:  # Verifica se o retângulo flutuante deve ser exibido
                     self.floating_rectangle.draw(frame)
                 cv2.imshow("Text Recognition", frame)
